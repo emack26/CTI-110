6894eae990c11a8c5753b6fea9753c917059f5d7
emack26 <macke1226@student.faytechcc.edu>
2026-09-18 12:36:03 -0400

    p2hw2 assignment
    editing and enhancing exiting programs

---
+++ p2hw2_MackElaine.py
@@ -0,0 +1,19 @@
+# Elaine Mack
+#09/18/2026
+# phw2_MackElaine.py
+# basic math operations
+
+total_budget = float(input("Enter your total budget: "))
+destination = input("Enter your travel destination: ")
+gas_cost = float(input("Enter the cost of gas: "))
+acconmodation_cost = float(input("Enter the cost of accommodation: "))
+food_cost = float(input("Enter the cost of food: "))
+
+total_expenses = gas_cost + acconmodation_cost + food_cost
+remaining_budget = total_budget - total_expenses
+
+print("\n------Travel Budget Summary----")
+print("destination:", destination)
+print("Total Budget: $", total_budget)
+print("Total Expenses: $", total_expenses)
+print("Remaining Budget: $", remaining_budget)
